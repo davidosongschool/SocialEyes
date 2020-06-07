60484b4131d1bb2901c7a08fa3b13af2d4663033
davidosongschool <davidosongschool@gmail.com>
2020-06-07 18:29:17 +0000

    Added sessions to register and redirect to dashboard - session checks added to each view

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import bcrypt
 import datetime
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from os import path
 if path.exists("env.py"):
     import env
@@ -15,35 +15,51 @@ app.config["MONGO_URI"] = os.environ.get('Mongo')
 
 mongo = PyMongo(app)
 
+users = mongo.db.users
+username = ''
+
 
 @app.route('/')
 @app.route('/landing')
 def landing():
-    # Check to see if the session is set - if it is then render dashboard -
-    # If not then give option to register or login
+    if 'username' in session:
+        return redirect(url_for('dashboard'))
+    return render_template('landing.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        user = users.find_one({'username': session['username']})
+        return render_template('dashboard.html', user=user)
     return render_template('landing.html')
 
 
 @app.route('/register_user', methods=['POST', 'GET'])
 def register_user():
+    if 'username' in session:
+        return redirect(url_for('dashboard'))
+    
     users = mongo.db.users
     encrypted = bcrypt.hashpw(
         request.form['password'].encode('utf-8'), bcrypt.gensalt())
     date = datetime.datetime.now()
     users.insert_one({'username': request.form['username'],
-                       'password': encrypted,
-                       'email': request.form['email'],
-                       'date_registered': date,
-                       'description': 'Tell Us About Yourself',
-                       'following': (1, 2, 3),
-                       'followers': (3, 2, 1),
-                       'discover': True,
-                       'avater': 'Choose a picture'})
+                        'password': encrypted,
+                        'email': request.form['email'],
+                        'date_registered': date,
+                        'description': 'Tell Us About Yourself',
+                        'following': (1, 2, 3),
+                        'followers': (3, 2, 1),
+                        'discover': True,
+                        'avater': 'Choose a picture'})
 
-    return render_template('dashboard.html', user = users.find_one({'username': request.form['username']}))
+    session['username'] = request.form['username']
+    return redirect(url_for('dashboard'))
 
 
 if __name__ == '__main__':
+    app.secret_key = 'Clodyoneill1'
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
