c67ee1c8f900e910a69c67728132fddaab9793e7
davidosongschool <davidosongschool@gmail.com>
2020-06-16 12:06:27 +0000

    desktop dashboard styled

--- static/js/nav.js
+++ static/js/nav.js
@@ -42,3 +42,10 @@ $(".find-users-menu-button").addClass("underline")
 $(".contain-following-users").addClass("d-none"); 
 });
 
+
+
+/* This code will be used for news posting to set the value of the url in the modal */ 
+$(".post-about-news").click(function(){
+/* get the value of the url in that news card and set it to the value of the url in the modal */
+$("#content_link").attr("value", $(this).attr('id'));
+});
